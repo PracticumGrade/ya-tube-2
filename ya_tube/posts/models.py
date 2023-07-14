87d9ee583486aa6eb31c84b48601d9671127d9f0
aeksei <aeksei94@gmail.com>
2023-07-14 16:44:02 +0300

    precode: удалено поле с картинкой

--- ya_tube/posts/models.py
+++ ya_tube/posts/models.py
@@ -18,7 +18,6 @@ class Post(models.Model):
     pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="posts", blank=True, null=True)
-    image = models.ImageField(upload_to='posts/', null=True, blank=True)  # поле для картинки
 
     def __str__(self):
         return self.text
